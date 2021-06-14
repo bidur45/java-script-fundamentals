7daf3bc3e0abbb56966f5583213ac4c283690b64
Your Name <[bidurparajuli946@gmail.com]>
2021-06-14 16:39:00 +0545

    third commit

---
+++ 5-precedence of operators/script.js
@@ -0,0 +1,24 @@
+// Math First, after then comparision.
+// Mathematical operators from left to right
+
+// console.log(50 - 5 - 10);
+
+//Assignment operators from right to left
+
+let a,b;
+a = b = 50 - 5 - 10;
+console.log(a, b);
+
+// grouping has highest precedence
+// Calculate Avarage of 2 people
+
+// a = 20;
+// b = 30;
+// console.log(a + b / 2);
+// console.log((a + b) / 2);
+
+// cosnt age = 20;
+
+
+
+    
