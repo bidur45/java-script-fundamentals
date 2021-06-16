a4c2db4776574346de7a08a7152086081c2aa71c
Your Name <[bidurparajuli946@gmail.com]>
2021-06-16 14:07:24 +0545

    function

---
+++ functions/script.js
@@ -0,0 +1,62 @@
+'use strict'
+// function myClass() {
+//   console.log("This is JavaScript Fundamentals part II");
+// }
+
+// Calling the function
+// Running the function
+// Invoking the function
+// myClass();
+
+
+// We can pass data to function
+// We can also receive data from function by returning the value
+
+// console.log(career('Facebook', 'Apple'));
+
+// function career(internship, job) {
+//   // console.log(internship, job);
+//   const myCareer = `I have done internship at ${internship} and job at ${job}`;
+//   return myCareer;
+// }
+
+
+
+
+//  function calcAge1(birthYear) {
+//   return 2021 - birthYear;
+  
+// }
+
+// const age = calcAge1(1991);
+
+// console.log(`I am ${age} year's old`);
+
+//  
+// const calcAge2 = function (birthYear) {
+//     return 2021 - birthYear;
+//   };
+  
+//   const age2 = calcAge2(1991);
+//   console.log(`I am ${age2} year's old`);
+  
+// const calcAge = function (birthYear) {
+//     return 2021 - birthYear;
+//   }
+  
+//   console.log(calcAge(2000));
+  
+
+
+//arrow function
+
+//   const calcAge1 = birthYear1 => 2021 - birthYear1;
+//   console.log(calcAge1(2000));
+  
+//   const marriageCalculator = (birthYear,age) => {
+//     const age = 2021 -birthYear;
+//     const marriaegAge = 30 - age;
+//     return marriaegAge;
+//   }
+  
+//   console.log(marriageCalculator(1992));
